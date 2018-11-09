7c9b285f5d46657331f9fc84bf270fd8ea9a34d1
karabarley <karabarleta@gmail.com>
2018-11-08 21:19:53 -0500

    Add firebase reference

--- src/routes/jobs/jobs.router.js
+++ src/routes/jobs/jobs.router.js
@@ -4,7 +4,7 @@ const { jobsController } = require("./jobs.controller");
 const router = express.Router();
 const db = require("../../utils/database.js");
 
-router.get("", jobsController);
+router.get("/", jobsController);
 router.post("", (req, res) => {
   const formData = req.body;
   console.log(req.body);
